171703ae16605633e5c5ad43d8c4f533c796e36f
hamish <hamishgeorgedavie@gmail.com>
2023-05-17 11:39:20 +1000

    Changes

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,25 @@
 require 'pry'
 
 class String
+   # attr_reader :sentence
 
   def sentence?
+        self.end_with?(".")
     
   end
 
   def question?
+        self.end_with?("?")
 
   end
 
   def exclamation?
+        self.end_with?("!")
 
   end
 
   def count_sentences
+        self.split(/[.!?]/).filter { |sentence| !sentence.empty? }.lengt
 
   end
 end
